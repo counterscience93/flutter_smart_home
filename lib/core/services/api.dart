80f9ae835af2e173f56b7a8cfacc1a59150f09ea
le.man <le.man7193@mgmail.com>
2019-10-21 10:10:22 +0700

    upload v1

--- lib/core/services/api.dart
+++ lib/core/services/api.dart
@@ -15,27 +15,22 @@ class Api {
     // Get user profile for id
     var response = await client.get('$endpoint/users/$userId');
     // check response.statusCode == 200 do ...
-    print('BODYYYY: ${response.body}');
     // Convert and return
     return User.fromRawJson(response.body);
   }
 
   Future<List<Device>> getDevicesByUserId(int userId) async {
     var devices = List<Device>();
-    print('URI: $endpoint/devices?userId=$userId');
     // Get list devices by user id
     var response = await client.get('$endpoint/devices?userId=$userId');
 
     // parse into List
     var parsed = json.decode(response.body) as List<dynamic>;
-    print('PARSED: ${parsed.toString()}');
     // loop and convert each item to Device
     // devices = List<Device>.from(parsed.map((x) => Device.fromRawJson(x.toString())));
     for (var device in parsed) {
-      print('DEVICEEEEE: ${device.toString()}');
       devices.add(Device.fromJson(device));
     }
-    print('DEVICES: ${devices.length}');
     return devices;
   }
 }
--- lib/ui/views/home_view.dart
+++ lib/ui/views/home_view.dart
@@ -5,54 +5,112 @@ import 'package:smart_home_final/ui/share/text_styles.dart';
 import 'package:smart_home_final/ui/share/ui_helpers.dart';
 import 'package:smart_home_final/ui/views/widget/device_view_widget.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
 
+class _HomeViewState extends State<HomeView> {
+  int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
-    User user = Provider.of<User>(context);
+    //User user = Provider.of<User>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Smart Home KHN'),
-      ),
-      drawer: Drawer(
-        child: Column(
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              accountName: new Text(
-                user.userName,
-                style:
-                    new TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
-              ),
-              accountEmail: new Text(
-                user.email,
-                style:
-                    new TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
-              ),
-            ),
-            // new Column(children: null),
-          ],
-        ),
-      ),
+      // appBar: PreferredSize(
+      //   preferredSize: Size(null, 80),
+      //   child: Container(
+      //     width: MediaQuery.of(context).size.width,
+      //     height: 80,
+      //     child: Container(
+      //       // color: Colors.blueAccent,
+      //       child: Container(
+      //         margin: EdgeInsets.fromLTRB(20, 30, 20, 0),
+      //         child: Row(
+      //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //           crossAxisAlignment: CrossAxisAlignment.center,
+      //           children: [
+      //             Icon(
+      //               Icons.info,
+      //               size: 30,
+      //               color: Colors.green,
+      //             ),
+      //             Text(
+      //               "KHSmart",
+      //               style: TextStyle(fontSize: 24, color: Colors.green),
+      //             ),
+      //             Icon(
+      //               Icons.add,
+      //               size: 30,
+      //               color: Colors.green,
+      //             ),
+      //           ],
+      //         ),
+      //       ),
+      //     ),
+      //   ),
+      // ),
+      // drawer: Drawer(
+      //   child: Column(
+      //     children: <Widget>[
+      //       UserAccountsDrawerHeader(
+      //         accountName: new Text(
+      //           user.userName,
+      //           style:
+      //               new TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+      //         ),
+      //         accountEmail: new Text(
+      //           user.email,
+      //           style:
+      //               new TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+      //         ),
+      //       ),
+      //       // new Column(children: null),
+      //     ],
+      //   ),
+      // ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          UIHelper.verticalSpaceLarge,
-          Padding(
-            padding: const EdgeInsets.only(left: 20.0),
-            child: Text(
-              'Welcome ${user.userName}',
-              style: headerStyle,
+          Container(
+            margin: EdgeInsets.fromLTRB(20, 30, 20, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.info,
+                  size: 30,
+                  color: Colors.green,
+                ),
+                Text(
+                  "KHSmart",
+                  style: TextStyle(fontSize: 24, color: Colors.green),
+                ),
+                Icon(
+                  Icons.add,
+                  size: 30,
+                  color: Colors.green,
+                ),
+              ],
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20.0),
-            child:
-                Text('Here are all your smart devices', style: subHeaderStyle),
-          ),
-          UIHelper.verticalSpaceSmall,
+          // UIHelper.verticalSpaceLarge,
+          // // Padding(
+          // //   padding: const EdgeInsets.only(left: 20.0),
+          // //   child: Text(
+          // //     'Welcome ${user.userName}',
+          // //     style: headerStyle,
+          // //   ),
+          // // ),
+          // // Padding(
+          // //   padding: const EdgeInsets.only(left: 20.0),
+          // //   child:
+          // //       Text('Here are all your smart devices', style: subHeaderStyle),
+          // // ),
+          // UIHelper.verticalSpaceSmall,
           Expanded(
             child: Devices(),
-          )
+          ),
         ],
       ),
     );
